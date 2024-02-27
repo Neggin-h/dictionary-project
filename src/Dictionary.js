154b8c2ca24392db71829e63056bcf8e3b114f3a
Neggin-h <negginhariri@gmail.com>
2024-02-27 16:17:25 +0330

    Added API url

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+
     event.preventDefault();
     alert(`Searching for ${keyword} definition`);
   }
