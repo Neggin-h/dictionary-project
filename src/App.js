4350ab3ba8de21eb5439b1a6bed5a41f2a541ce2
Neggin-h <negginhariri@gmail.com>
2024-02-27 15:56:20 +0330

    Created the basic search form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import logo from "./Logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="Logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="footer">
+          <small>Codded by Neggin Hariri</small>
+        </footer>
+      </div>
     </div>
   );
 }
