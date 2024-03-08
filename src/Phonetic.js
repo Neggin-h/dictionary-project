5afa71f1f9428977e37c6f19caa25d7757b16c01
Neggin-h <negginhariri@gmail.com>
2024-03-08 18:09:07 +0330

    Fixed an issue with the phonetics

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,10 +3,7 @@ import React from "react";
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        Listen
-      </a>
-      <span className="text">{props.phonetic.text}</span>
+      <span className="text">{props.phonetic}</span>
     </div>
   );
 }
